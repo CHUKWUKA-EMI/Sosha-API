63bc8ff62fd0be1d81373f2616c9db376e8bc9a0
Chukwuka Emi <emichukwuka@gmail.com>
2020-11-22 23:49:36 +0100

    added subscriptions

--- src/DB/connection.js
+++ src/DB/connection.js
@@ -3,9 +3,9 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const { DEV_DATABASE_URL, LOCAL_DB } = process.env;
+const { DATABASE_URL, LOCAL_DB } = process.env;
 
-const sequelize = new Sequelize(DEV_DATABASE_URL, {
+const sequelize = new Sequelize(DATABASE_URL, {
   dialect: "postgres",
   host: "lallah.db.elephantsql.com",
   define: {
